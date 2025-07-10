ceb17de9ef442225d8d10e762e0aab8fbef98a3e
Kaung-Htet-Hein <kaunghtethein.dev@gmail.com>
2025-07-10 20:50:02 +0800

    Initial Commit

---
+++ crypto-tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  crypto-tracker
+//
+//  Created by Kaung Htet Hein on 10/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ crypto-tracker/crypto_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_trackerApp.swift
+//  crypto-tracker
+//
+//  Created by Kaung Htet Hein on 10/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct crypto_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
